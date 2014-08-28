89d47b19e3bc5fed87aeb86bd14227e6204b85fd
Victor Apihtin <vapyhtin@gmail.com>
2014-08-28 18:38:44 +0200

    being tired

--- Assets/Executor.cs
+++ Assets/Executor.cs
@@ -10,273 +10,133 @@ using java.util;
 
 public class Executor : MonoBehaviour 
 {
+	public Transform pacmanTransform;
+	public Transform[] ghostsTransforms;
+
+	private Material pillMaterial;
+
 	Game game = new Game(0L);
 
 	Controller pacManController = new StarterPacMan();
 	Controller ghostController = new StarterGhosts();
 
+	private GameObject[] pills;
+	private GameObject[] powerPills;
+
+	Constants.GHOST[] ghosts = new Constants.GHOST[]
+	{
+		Constants.GHOST.BLINKY,
+     	Constants.GHOST.INKY,
+     	Constants.GHOST.PINKY,
+		Constants.GHOST.SUE
+	};
+
+	string[] ghostNames = new string[]
+	{
+		"blinky",
+		"inky",
+		"pinky",
+		"sue"
+	};
+
 	IEnumerator Start()
 	{
-		float delay = Time.deltaTime;
+		float delay = 0.25f;
+
+		pillMaterial = new Material( Shader.Find("Self-Illumin/VertexLit") );
+		pillMaterial.color = Color.yellow;
+
+		pills = new GameObject[game.getCurrentMaze().pillIndices.Length];
+		for( int i = 0; i < game.getCurrentMaze().pillIndices.Length; i++ )
+		{
+			int nodeIdx = game.getCurrentMaze().pillIndices[i];
+
+			GameObject pill = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			pill.transform.position = new Vector3(game.getCurrentMaze().graph[nodeIdx].x, game.getCurrentMaze().graph[nodeIdx].y, 0f);
+
+			pill.renderer.sharedMaterial = pillMaterial;
+
+			pills[i] = pill;
+		}
+
+		powerPills = new GameObject[game.getCurrentMaze().powerPillIndices.Length];
+		for( int i = 0; i < game.getCurrentMaze().powerPillIndices.Length; i++ )
+		{
+			int nodeIdx = game.getCurrentMaze().powerPillIndices[i];
+			
+			GameObject pill = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			pill.transform.position = new Vector3(game.getCurrentMaze().graph[nodeIdx].x, game.getCurrentMaze().graph[nodeIdx].y, 0f);
+			pill.transform.localScale = Vector3.one * 3f;
+
+			pill.renderer.sharedMaterial = pillMaterial;
+
+			powerPills[i] = pill;
+		}
 
 		while (!game.gameOver())
 		{
 			game.advanceGame((Constants.MOVE)pacManController.getMove(game.copy(), -1L), (EnumMap)ghostController.getMove(game.copy(), -1L));
+			UpdateVisuals();
+
 			yield return new WaitForSeconds(delay);
 		}
 	}
 
-//	public static void saveToFile(string data, string name, bool append)
-//	{
-//			FileOutputStream fileOutputStream = new FileOutputStream(name, append);
-//			PrintWriter printWriter = new PrintWriter(fileOutputStream);
-//			printWriter.println(data);
-//			printWriter.flush();
-//			fileOutputStream.close();
-//
-//	}
+	public void UpdateVisuals()
+	{
+		int pacmanNode = game.getPacmanCurrentNodeIndex();
+		pacmanTransform.transform.position = new Vector3(game.getCurrentMaze().graph[pacmanNode].x, game.getCurrentMaze().graph[pacmanNode].y, 0f);
 
-//	public virtual void replayGame(string fileName, bool visual)
-//	{
-//		ArrayList arrayList = Executor.loadReplay(fileName);
-//		Game game = new Game(0L);
-//		GameView gameView = null;
-//		if (visual)
-//		{
-//			gameView = new GameView(game).showGame();
-//		}
-//		int i = 0;
-//		while (i < arrayList.size())
-//		{
-//			game.setGameState((string)arrayList.get(i));
-//			InterruptedException ex;
-//			try
-//			{
-//				Thread.sleep((long)((ulong)40));
-//			}
-//			catch (InterruptedException arg_52_0)
-//			{
-//				ex = ByteCodeHelper.MapException<InterruptedException>(arg_52_0, ByteCodeHelper.MapFlags.NoRemapping);
-//				goto IL_5D;
-//			}
-//		IL_6C:
-//				if (visual)
-//			{
-//				gameView.repaint();
-//			}
-//			i++;
-//			continue;
-//			goto IL_6C;
-//		IL_5D:
-//				InterruptedException this2 = ex;
-//			Throwable.instancehelper_printStackTrace(this2);
-//			goto IL_6C;
-//		}
-//	}
-//
-//	public virtual void runExperiment(Controller pacManController, Controller ghostController, int trials)
-//	{
-//		double num = (double)0f;
-//		Random random = new Random(0L);
-//		for (int i = 0; i < trials; i++)
-//		{
-//			Game.__<clinit>();
-//			Game game = new Game(random.nextLong());
-//			while (!game.gameOver())
-//			{
-//				game.advanceGame((Constants.MOVE)pacManController.getMove(game.copy(), System.currentTimeMillis() + (long)((ulong)40)), (EnumMap)ghostController.getMove(game.copy(), System.currentTimeMillis() + (long)((ulong)40)));
-//			}
-//			num += (double)game.getScore();
-//			System.@out.println(new StringBuilder().append(i).append("\t").append(game.getScore()).toString());
-//		}
-//		System.@out.println(num / (double)trials);
-//	}
-//
-//
-//	public virtual void runGame(Controller pacManController, Controller ghostController, bool visual, int delay)
-//	{
-//		Game game = new Game(0L);
-//		GameView gameView = null;
-//		if (visual)
-//		{
-//			gameView = new GameView(game).showGame();
-//		}
-//		while (!game.gameOver())
-//		{
-//			game.advanceGame((Constants.MOVE)pacManController.getMove(game.copy(), -1L), (EnumMap)ghostController.getMove(game.copy(), -1L));
-//			try
-//			{
-//				Thread.sleep((long)delay);
-//			}
-//			catch (Exception arg_5F_0)
-//			{
-//				if (ByteCodeHelper.MapException<Exception>(arg_5F_0, ByteCodeHelper.MapFlags.Unused) == null)
-//				{
-//					throw;
-//				}
-//			}
-//		IL_72:
-//				if (visual)
-//			{
-//				gameView.repaint();
-//				continue;
-//			}
-//			continue;
-//			goto IL_72;
-//		}
-//	}
-//
-//	public virtual void runGameTimed(Controller pacManController, Controller ghostController, bool visual)
-//	{
-//		Game game = new Game(0L);
-//		GameView gameView = null;
-//		if (visual)
-//		{
-//			gameView = new GameView(game).showGame();
-//		}
-//		if (pacManController is HumanController)
-//		{
-//			gameView.getFrame().addKeyListener(((HumanController)pacManController).getKeyboardInput());
-//		}
-//		new Thread(pacManController).start();
-//		new Thread(ghostController).start();
-//		while (!game.gameOver())
-//		{
-//			pacManController.update(game.copy(), System.currentTimeMillis() + (long)((ulong)40));
-//			ghostController.update(game.copy(), System.currentTimeMillis() + (long)((ulong)40));
-//			InterruptedException ex;
-//			try
-//			{
-//				Thread.sleep((long)((ulong)40));
-//			}
-//			catch (InterruptedException arg_91_0)
-//			{
-//				ex = ByteCodeHelper.MapException<InterruptedException>(arg_91_0, ByteCodeHelper.MapFlags.NoRemapping);
-//				goto IL_9B;
-//			}
-//		IL_A9:
-//				game.advanceGame((Constants.MOVE)pacManController.getMove(), (EnumMap)ghostController.getMove());
-//			if (visual)
-//			{
-//				gameView.repaint();
-//				continue;
-//			}
-//			continue;
-//			goto IL_A9;
-//		IL_9B:
-//				InterruptedException this2 = ex;
-//			Throwable.instancehelper_printStackTrace(this2);
-//			goto IL_A9;
-//		}
-//		pacManController.terminate();
-//		ghostController.terminate();
-//	}
-//
-//	public virtual void runGameTimedRecorded(Controller pacManController, Controller ghostController, bool visual, string fileName)
-//	{
-//		StringBuilder stringBuilder = new StringBuilder();
-//		Game game = new Game(0L);
-//		GameView gameView = null;
-//		if (visual)
-//		{
-//			gameView = new GameView(game).showGame();
-//			if (pacManController is HumanController)
-//			{
-//				gameView.getFrame().addKeyListener(((HumanController)pacManController).getKeyboardInput());
-//			}
-//		}
-//		new Thread(pacManController).start();
-//		new Thread(ghostController).start();
-//		while (!game.gameOver())
-//		{
-//			pacManController.update(game.copy(), System.currentTimeMillis() + (long)((ulong)40));
-//			ghostController.update(game.copy(), System.currentTimeMillis() + (long)((ulong)40));
-//			InterruptedException ex;
-//			try
-//			{
-//				Thread.sleep((long)((ulong)40));
-//			}
-//			catch (InterruptedException arg_97_0)
-//			{
-//				ex = ByteCodeHelper.MapException<InterruptedException>(arg_97_0, ByteCodeHelper.MapFlags.NoRemapping);
-//				goto IL_A2;
-//			}
-//		IL_B1:
-//				game.advanceGame((Constants.MOVE)pacManController.getMove(), (EnumMap)ghostController.getMove());
-//			if (visual)
-//			{
-//				gameView.repaint();
-//			}
-//			stringBuilder.append(new StringBuilder().append(game.getGameState()).append("\n").toString());
-//			continue;
-//			goto IL_B1;
-//		IL_A2:
-//				InterruptedException this2 = ex;
-//			Throwable.instancehelper_printStackTrace(this2);
-//			goto IL_B1;
-//		}
-//		pacManController.terminate();
-//		ghostController.terminate();
-//		Executor.saveToFile(stringBuilder.toString(), fileName, false);
-//	}
-//
-//	public virtual void runGameTimedSpeedOptimised(Controller pacManController, Controller ghostController, bool fixedTime, bool visual)
-//	{
-//		Game game = new Game(0L);
-//		GameView gameView = null;
-//		if (visual)
-//		{
-//			gameView = new GameView(game).showGame();
-//		}
-//		if (pacManController is HumanController)
-//		{
-//			gameView.getFrame().addKeyListener(((HumanController)pacManController).getKeyboardInput());
-//		}
-//		new Thread(pacManController).start();
-//		new Thread(ghostController).start();
-//		while (!game.gameOver())
-//		{
-//			pacManController.update(game.copy(), System.currentTimeMillis() + (long)((ulong)40));
-//			ghostController.update(game.copy(), System.currentTimeMillis() + (long)((ulong)40));
-//			InterruptedException ex;
-//			try
-//			{
-//				int num = 40;
-//				for (int i = 0; i < 40; i++)
-//				{
-//					Thread.sleep(1L);
-//					if (pacManController.hasComputed() && ghostController.hasComputed())
-//					{
-//						num = i;
-//						break;
-//					}
-//				}
-//				if (fixedTime)
-//				{
-//					Thread.sleep((long)((40 - num) * 1));
-//				}
-//				game.advanceGame((Constants.MOVE)pacManController.getMove(), (EnumMap)ghostController.getMove());
-//			}
-//			catch (InterruptedException arg_EA_0)
-//			{
-//				ex = ByteCodeHelper.MapException<InterruptedException>(arg_EA_0, ByteCodeHelper.MapFlags.NoRemapping);
-//				goto IL_F5;
-//			}
-//		IL_104:
-//				if (visual)
-//			{
-//				gameView.repaint();
-//				continue;
-//			}
-//			continue;
-//			goto IL_104;
-//		IL_F5:
-//				InterruptedException this2 = ex;
-//			Throwable.instancehelper_printStackTrace(this2);
-//			goto IL_104;
-//		}
-//		pacManController.terminate();
-//		ghostController.terminate();
-//	}
+			
+		Constants.MOVE pacmanmove = game.getPacmanLastMoveMade();
+
+		if( pacmanmove == Constants.MOVE.DOWN )
+			pacmanTransform.animation.Play( "pacman" + "_down" );
+		else	
+			if( pacmanmove == Constants.MOVE.UP )
+				pacmanTransform.animation.Play( "pacman" + "_up" );
+		else	
+			if( pacmanmove == Constants.MOVE.LEFT )
+				pacmanTransform.animation.Play( "pacman" + "_left" );
+		else	
+			if( pacmanmove == Constants.MOVE.RIGHT )
+				pacmanTransform.animation.Play( "pacman" + "_right" );
+
+
+		int[] ghostsNodes = new int[4]; 
+		for( int i = 0; i < 4; i++ )
+		{
+			Constants.MOVE move = game.getGhostLastMoveMade( ghosts[i] );
+			
+			if( move == Constants.MOVE.DOWN )
+				ghostsTransforms[i].animation.Play( ghostNames[i] + "_down" );
+			else	
+			if( move == Constants.MOVE.UP )
+				ghostsTransforms[i].animation.Play( ghostNames[i] + "_up" );
+			else	
+			if( move == Constants.MOVE.LEFT )
+				ghostsTransforms[i].animation.Play( ghostNames[i] + "_left" );
+			else	
+			if( move == Constants.MOVE.RIGHT )
+				ghostsTransforms[i].animation.Play( ghostNames[i] + "_right" );
+
+			ghostsNodes[i] = game.getGhostCurrentNodeIndex(ghosts[i]);
+		}
+
+
+		for( int i = 0; i < 4; i ++ )
+			ghostsTransforms[i].transform.position = new Vector3(game.getCurrentMaze().graph[ghostsNodes[i]].x, game.getCurrentMaze().graph[ghostsNodes[i]].y, 0f);
+
+		if( game.wasPillEaten() )
+		{
+			pills[ game.getPillIndex(pacmanNode) ].renderer.enabled = false;
+		}
+
+		if( game.wasPowerPillEaten() )
+		{
+			powerPills[ game.getPowerPillIndex(pacmanNode) ].renderer.enabled = false;
+		}
+	}
+
 
 }
