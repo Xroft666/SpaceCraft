7a0a405a90e82141e3d39574606a941116e565f6
Victor Apihtin <vapyhtin@gmail.com>
2015-05-08 00:06:39 +0200

    creation funcs

--- Assets/Scripts/BehaviourScheme/BSExit.cs
+++ Assets/Scripts/BehaviourScheme/BSExit.cs
@@ -1,7 +1,20 @@
-﻿namespace BehaviourScheme
+﻿
+using SpaceSandbox;
+
+namespace BehaviourScheme
 {
 	public class BSExit : BSNode 
 	{
-		
+		private BlueprintScheme m_scheme = null;
+
+		public void SetScheme( BlueprintScheme scheme )
+		{
+			m_scheme = scheme;
+		}
+
+		public override void Activate()
+		{
+			m_scheme.OnExitNode( this );
+		}
 	}
 }
--- Assets/Scripts/UserCreatedAgency/BlueprintScheme.cs
+++ Assets/Scripts/UserCreatedAgency/BlueprintScheme.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using BehaviourScheme;
 
@@ -6,13 +7,72 @@ namespace SpaceSandbox
 {
 	public class BlueprintScheme : Entity 
 	{
-		private BSState m_defaultState;
+		private List<BSEntry> m_entries = new List<BSEntry>();
+		private List<BSState> m_states = new List<BSState>();
+		private List<BSExit> m_exits = new List<BSExit>();
 
-		private List<BSEntry> m_entries;
-		private List<BSState> m_states;
-		private List<BSExit> m_exits;
+
+		private List<BSNode> m_nodes = new List<BSNode>();
+
+		public void CreateState()
+		{
+			BSState node = new BSState();
+
+			m_states.Add( node );
+			m_nodes.Add( node );
+		}
+
+		public void CreateAction( Action action )
+		{
+			BSAction node = new BSAction();
+			node.SetAction( action );
+
+			m_nodes.Add( node );
+		}
+		
+		public void CreateEntry(  )
+		{
+			BSEntry node = new BSEntry();
+
+			m_entries.Add( node );
+			m_nodes.Add( node );
+		}
+		
+		public void CreateExit()
+		{
+			BSExit node = new BSExit();
+			node.SetScheme( this );
+
+			m_exits.Add( node );
+			m_nodes.Add( node );
+		}
+
+		public void CreateSelect()
+		{
+			BSSelect node = new BSSelect();
+			m_nodes.Add( node );
+		}
+
+		public void CreateEvaluate()
+		{
+			BSEvaluate node = new BSEvaluate();
+			m_nodes.Add( node );
+		}
+
+
+		public void ConnectElements( BSNode left, BSNode right )
+		{
+			left.AddChild( right );
+			m_nodes.Add( node );
+		}
+
 
 		public void UpdateScheme()
+		{
+
+		}
+
+		public void OnExitNode( BSNode exitNode )
 		{
 
 		}
