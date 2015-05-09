12ceb5fc4ecb9bdc91dafecbd37c5f0a7184e365
Victor Apihtin <vapyhtin@gmail.com>
2015-05-10 01:29:14 +0200

    Planner, maybe

--- Assets/Scripts/Sandbox/SandboxAbstraction/Device.cs
+++ Assets/Scripts/Sandbox/SandboxAbstraction/Device.cs
@@ -1,24 +1,76 @@
-﻿
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
+using UnityEngine;
 
 namespace SpaceSandbox
 {
 	[System.Serializable]
 	public class Device : Entity 
 	{
-		public string deviceName = "Unknown";
-		public List<Resource> resources = new List<Resource>();
+		protected BlueprintScheme blueprint = null;
 
-		public BlueprintScheme blueprint = null;
+		protected List<Device> m_integratedDevices = new List<Device>();
+		protected Dictionary<string, Job> m_functions = new Dictionary<string, Job>();
+		protected Dictionary<string, Action> m_events = new Dictionary<string, Action>();
 
-		public delegate void DeviceCallback( object[] output );
-		public DeviceCallback outputCallback;
 
-		public virtual void OnStart(params object[] input){}
-		public virtual void OnUpdate(){}
-		public virtual void OnDelete(){}
+
+		public void AddFunction ( string name, Job function )
+		{
+			m_functions[name] = function;
+		}
+
+		public Job GetFunction ( string name )
+		{
+			return m_functions[name];
+		}
+
+		public void RemoveFunction ( string name )
+		{
+			m_functions.Remove( name );
+		}
+
+		public void AddEvent ( string name, Action action )
+		{
+			m_events[name] = action;
+		}
+
+		public Action GetEvent( string name )
+		{
+			return m_events[name];
+		}
 		
-		public virtual void OnActivate(params object[] input){}
-		public virtual void OnDeactivate(params object[] input){}
+		public void RemoveEvent ( string name )
+		{
+			m_events.Remove( name );
+		}
+
+
+
+
+		public void Activate( string functionName )
+		{
+			m_functions[functionName].start();
+		}
+
+		public IEnumerator ActivateAndWait( string functionName )
+		{
+			yield return m_functions[functionName].startAsCoroutine();
+		}
+
+
+//		public List<Resource> resources = new List<Resource>();
+
+//		public delegate void DeviceCallback( object[] output );
+//		public DeviceCallback outputCallback;
+//
+//		public virtual void OnStart(params object[] input){}
+//		public virtual void OnUpdate(){}
+//		public virtual void OnDelete(){}
+//		
+//		public virtual void OnActivate(params object[] input){}
+//		public virtual void OnDeactivate(params object[] input){}
 	}
 }
--- Assets/Scripts/UserCreatedAgency/BlueprintScheme.cs
+++ Assets/Scripts/UserCreatedAgency/BlueprintScheme.cs
@@ -1,6 +1,7 @@
-﻿
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+
 using BehaviourScheme;
 
 namespace SpaceSandbox
@@ -14,6 +15,8 @@ namespace SpaceSandbox
 
 		private List<BSNode> m_nodes = new List<BSNode>();
 
+		private List<Job> m_plannedActions = new List<Job>();
+
 		public void CreateState()
 		{
 			BSState node = new BSState();
@@ -22,10 +25,11 @@ namespace SpaceSandbox
 			m_nodes.Add( node );
 		}
 
-		public void CreateAction( Action action )
+		public void CreateAction( Device device, string functionName )
 		{
 			BSAction node = new BSAction();
-			node.SetAction( action );
+
+			node.SetAction( () => device.Activate(functionName) );
 
 			m_nodes.Add( node );
 		}
@@ -63,11 +67,43 @@ namespace SpaceSandbox
 		public void ConnectElements( BSNode left, BSNode right )
 		{
 			left.AddChild( right );
-			m_nodes.Add( node );
 		}
 
 
 		public void UpdateScheme()
+		{
+
+		}
+
+		public void AddActionToPlanner( Job job )
+		{
+			m_plannedActions.Add( job );
+		}
+
+		public void AddActionToPlanner( Device device, string functionName )
+		{
+			m_plannedActions.Add( device.GetFunction( functionName ));
+		}
+
+		public void ExecutePlanner()
+		{
+			// a planner can be interrupted if (high priority event kicks in?)
+			// a planner can be interrupted if an action (or a check) fails?
+
+			Job.make( ActionsExcecution(), true );
+		}
+
+		private IEnumerator ActionsExcecution()
+		{
+			foreach( Job job in m_plannedActions )
+			{
+				yield return job.startAsCoroutine();
+			}
+
+			OnPlannerCompleted();
+		}
+
+		public void OnPlannerCompleted()
 		{
 
 		}
